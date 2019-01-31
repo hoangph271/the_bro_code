af4d57dc16e1623daa5a77699d3637d5245a5801
orvjund <orvjund@gmail.com>
2019-02-01 02:00:38 +0700

    Add more songs, and misses

--- pages/waiting-for-love/script.js
+++ pages/waiting-for-love/script.js
@@ -1,81 +1,86 @@
-const D_DAY = new Date(2019, 1, 10, 16, 00, 00);
+const D_DAY = new Date(2019, 1, 10, 16, 0, 0);
+const MISSES = [
+  {
+    id: 1,
+    her: 'I miss your smell',
+    me: 'I miss your breath...!',
+    src: './A Thousand Years - Christina Perri (NhacPro.net).mp3',
+    title: 'A thousand years - Christina Perri',
+  },
+  {
+    id: 2,
+    me: `I miss the way you turn away and cover your head when you're shy...!`,
+  },
+];
+/* global Vue */
+new Vue({
+  el: '#app',
+  data: {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    paused: true,
+    misses: MISSES,
+    audio: new Audio(),
+    srcId: 0,
+    title: 'Back To You Countdown',
+  },
+  mounted: function() {
+    this.updateTimer();
+  },
+  methods: {
+    updateTimer: function() {
+      const now = new Date();
+      const timeLeft = this.getTimeLeft(now, D_DAY);
+      this.days = timeLeft.days;
+      this.hours = timeLeft.hours;
+      this.minutes = timeLeft.minutes;
+      this.seconds = timeLeft.seconds;
+      if (now.getTime() < D_DAY.getTime()) {
+        setTimeout(this.updateTimer, 1000);
+      } else {
+        // TODO: Set final text
+      }
+    },
+    getTimeLeft: function (from, to) {
+      const distance = to.getTime() - from.getTime();
+      const days = this.paddedTwo(Math.floor(distance / (1000 * 60 * 60 * 24)));
+      const hours = this.paddedTwo(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+      const minutes = this.paddedTwo(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
+      const seconds = this.paddedTwo(Math.floor((distance % (1000 * 60)) / 1000));
 
-const countdownClock = document.querySelector('.clock');
+      return {
+        days,
+        hours,
+        minutes,
+        seconds,
+      };
+    },
+    paddedTwo: function (number) {
+      return String(number).padStart(2, '0');
+    },
+    toggleMusic: function({ id, src, title }) {
+      if (!id || !src) return;
 
+      if (id != this.srcId) {
+        this.srcId = id;
+        this.audio.src = src;
+        this.audio.play();
+        this.paused = false;
+        this.title = title;
+        return;
+      }
 
-const updateClockContent = (() => {
-  const daysElem = countdownClock.querySelector('.days');
-  const hoursElem = countdownClock.querySelector('.hours');
-  const minutesElem = countdownClock.querySelector('.minutes');
-  const secondsElem = countdownClock.querySelector('.seconds');
-
-  return (timeLeft) => {
-    daysElem.querySelector('.value').innerHTML = timeLeft.days;
-    if (timeLeft.days <= 1) {
-      daysElem.querySelector('.label').innerHTML = 'day';
-    } else {
-      daysElem.querySelector('.label').innerHTML = 'days';
+      if (this.paused) {
+        this.audio.play();
+        this.paused = false;
+        this.title = title;
+      } else {
+        this.audio.pause();
+        this.paused = true;
+        this.title = 'Back To You Countdown';
+      }
     }
-    hoursElem.querySelector('.value').innerHTML = timeLeft.hours;
-    if (timeLeft.hours <= 1) {
-      hoursElem.querySelector('.label').innerHTML = 'hour';
-    } else {
-      hoursElem.querySelector('.label').innerHTML = 'hours';
-    }
-    minutesElem.querySelector('.value').innerHTML = timeLeft.minutes;
-    if (timeLeft.minutes <= 1) {
-      minutesElem.querySelector('.label').innerHTML = 'minute';
-    } else {
-      minutesElem.querySelector('.label').innerHTML = 'minutes';
-    }
-    secondsElem.querySelector('.value').innerHTML = timeLeft.seconds;
-    if (timeLeft.seconds <= 1) {
-      secondsElem.querySelector('.label').innerHTML = 'second';
-    } else {
-      secondsElem.querySelector('.label').innerHTML = 'seconds';
-    }
-  };
-})();
-
-const audio = new Audio('./A Thousand Years - Christina Perri (NhacPro.net).mp3');
-document.querySelector('.song-button').addEventListener('click', function(e) {
-  if (this.classList.contains('paused')) {
-    audio.play();
-    this.classList.remove('paused');
-  } else {
-    audio.pause();
-    this.classList.add('paused');
   }
 });
-
-function refreshTimer() {
-  const now = new Date();
-  if (now.getTime() < D_DAY.getTime()) {
-    const timeLeft = getTimeLeft(now, D_DAY);
-    updateClockContent(timeLeft);
-    setTimeout(refreshTimer, 1000);
-  } else {
-    // TODO: Set final text
-    clearInterval(countdownInterval);
-  }
-};
-refreshTimer();
-
-function getTimeLeft(from, to) {
-  const distance = to.getTime() - from.getTime();
-  const days = paddedTwo(Math.floor(distance / (1000 * 60 * 60 * 24)));
-  const hours = paddedTwo(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const minutes = paddedTwo(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
-  const seconds = paddedTwo(Math.floor((distance % (1000 * 60)) / 1000));
-
-  return {
-    days,
-    hours,
-    minutes,
-    seconds,
-  }
-}
-
-function paddedTwo(number) {
-  return String(number).padStart(2, '0');
-}
---
+++ pages/waiting-for-love/vue.js
@@ -0,0 +1,10 @@
+/* global Vue */
+new Vue({
+  el: '#app',
+  data: {
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  },
+});
